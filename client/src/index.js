8285f293a344122dc1ac039c277b2f1f30afa2ff
dreak603 <dreak603@gmail.com>
2019-10-09 15:53:46 +0800

    add several package for lint and add exact path for App

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,22 @@
+import 'babel-core/register';
+import 'babel-polyfill';
 import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Switch, Route } from 'react-router-dom';
 import axios from 'axios';
 import App from './components/App';
+import history from './history';
 
 window.axios = axios;
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+  <Router history={history}>
+    <Switch>
+      <Route exact path="/">
+        <App />
+      </Route>{' '}
+    </Switch>{' '}
+  </Router>,
+  document.querySelector('#root')
+);
--- index.js
+++ index.js
@@ -3,44 +3,45 @@ const mongoose = require('mongoose');
 const path = require('path');
 const bodyParser = require('body-parser');
 
-//Connect to MongoDB
+// Connect to MongoDB
 mongoose.Promise = global.Promise;
 mongoose.set('debug', true);
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/shortenUrl', {
-    keepAlive: true,
-    reconnectTries: Number.MAX_VALUE,
-    useMongoClient: true
+  keepAlive: true,
+  reconnectTries: Number.MAX_VALUE,
+  useMongoClient: true
 });
 
 require('./models');
+
 const app = express();
 
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Content-type,Accept,x-access-token,X-Key'
-    );
-    if (req.method == 'OPTIONS') {
-        res.status(200).end();
-    } else {
-        next();
-    }
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-type,Accept,x-access-token,X-Key'
+  );
+  if (req.method == 'OPTIONS') {
+    res.status(200).end();
+  } else {
+    next();
+  }
 });
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-
 require('./routes/route')(app);
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/build/index.html'));
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 require('./services');
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Server started on port`, PORT);
+  console.log(`Server started on port`, PORT);
 });
